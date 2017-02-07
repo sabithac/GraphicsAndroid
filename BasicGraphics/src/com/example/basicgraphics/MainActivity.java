f127aa92c589eeba44545dd3296811f3704ab27b
ITLAB <ITLAB@IT>
2017-02-07 14:09:18 +0530

    Fifth Commit

--- BasicGraphics/src/com/example/basicgraphics/MainActivity.java
+++ BasicGraphics/src/com/example/basicgraphics/MainActivity.java
@@ -24,21 +24,19 @@ public class MainActivity extends Activity {
 			super(mcontext);
 		}
     @Override
+    protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		Paint paint = new Paint();
+		paint.setStyle(Paint.Style.FILL);
+		paint.setColor(Color.CYAN);
+		canvas.drawPaint(paint);
+    }
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    
     }
 }
