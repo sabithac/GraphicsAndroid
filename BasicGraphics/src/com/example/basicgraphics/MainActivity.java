cac50a0115f9ba570a198fdc3653c0349231454c
ITLAB <ITLAB@IT>
2017-02-07 19:18:11 +0530

    Third Commit

--- BasicGraphics/src/com/example/basicgraphics/MainActivity.java
+++ BasicGraphics/src/com/example/basicgraphics/MainActivity.java
@@ -7,13 +7,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
+	DemoView demo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        demo=new DemoView(this);
         setContentView(R.layout.activity_main);
     }
 
