7c45f9cb0c008ebfacb3bcbb7ad928a8cb1d29b0
ITLAB <ITLAB@IT>
2017-02-07 16:26:16 +0530

    Eigth Commit

--- BasicGraphics/src/com/example/basicgraphics/MainActivity.java
+++ BasicGraphics/src/com/example/basicgraphics/MainActivity.java
@@ -1,59 +1,60 @@
 package com.example.basicgraphics;
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-
-public class MainActivity extends Activity {
-
-	DemoView demo;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		demo = new DemoView(this);
-		setContentView(demo);
-	}
-
-	private class DemoView extends View {
-		public DemoView(Context mcontext) {
-			super(mcontext);
-		}
-
-		@Override
-		protected void onDraw(Canvas canvas) {
-			super.onDraw(canvas);
-
-			Paint paint = new Paint();
-			paint.setStyle(Paint.Style.FILL);
-
-			paint.setColor(Color.CYAN);
-			canvas.drawPaint(paint);
-
-			paint.setAntiAlias(false);
-			paint.setColor(Color.MAGENTA);
-			canvas.drawCircle(20, 20, 15, paint);
-			paint.setAntiAlias(false);
-			paint.setColor(Color.MAGENTA);
-			canvas.drawLine(10, 10,0,0, paint);
-
-			
-			paint.setAntiAlias(false);
-			paint.setColor(Color.RED);
-			canvas.drawRect(75,15, 100, 30, paint);
-
-			
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-}
+import android.os.Bundle;  
+import android.app.Activity;  
+import android.view.Menu;  
+import android.content.Context;  
+import android.graphics.Canvas;  
+import android.graphics.Color;  
+import android.graphics.Paint;  
+import android.view.View;  
+  
+public class MainActivity extends Activity {  
+  
+    DemoView demoview;  
+    @Override  
+    public void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        demoview = new DemoView(this);  
+        setContentView(demoview);  
+    }  
+  
+    private class DemoView extends View{  
+        public DemoView(Context context){  
+            super(context);  
+        }  
+  
+        @Override protected void onDraw(Canvas canvas) {  
+            super.onDraw(canvas);  
+  
+            Paint paint = new Paint();  
+            paint.setStyle(Paint.Style.FILL);  
+  
+            paint.setColor(Color.WHITE);  
+            canvas.drawPaint(paint);  
+              
+            paint.setAntiAlias(false);  
+            paint.setColor(Color.BLUE);  
+            canvas.drawCircle(10, 20, 15, paint);  
+  
+            
+            paint.setAntiAlias(true);  
+            paint.setColor(Color.GREEN);  
+            canvas.drawLine(60, 20, 0,0, paint);  
+  
+            paint.setAntiAlias(false);  
+            paint.setColor(Color.RED);  
+            canvas.drawRect(100, 5, 200, 30, paint);  
+                           
+            canvas.rotate(-45);  
+                      
+            
+              
+            canvas.restore();  
+        }  
+    }  
+    @Override  
+    public boolean onCreateOptionsMenu(Menu menu) {  
+        getMenuInflater().inflate(R.menu.main, menu);  
+        return true;  
+    }  
+}  
